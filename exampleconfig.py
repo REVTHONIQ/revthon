1973f61281b27c7166afe708410518af88f74c1b
bodythoniq <123871565+bodythoniq@users.noreply.github.com>
2023-01-30 13:15:51 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,19 +3,19 @@ from sample_config import Config
 
 class Development(Config):
 
-    APP_ID = 
+    APP_ID = 29112329
     
-    API_HASH = ""
+    API_HASH = "c99a62ee1c3c5afc3490cfaf7bd52d22"
 
-    ALIVE_NAME = ""
+    ALIVE_NAME = "TIMO"
 
-    DB_URI = ""
+    DB_URI = "postgres://sulwofpj:yxoskMUv6cyBEaMuavtQsNia2HeOcG6K@mouse.db.elephantsql.com/sulwofpj"
 
-    STRING_SESSION = ""
+    STRING_SESSION = "1ApWapzMBuxC7W_gA5EwROFY-gV8sR8fgfk59VZwnRQHgQvmoHisHlFBx9XXQvMVeWCdLXLIZuQXZ_aQ4scf99cIhJHncujNoAAvLn9Jlm9-LBi-qOIXXnjahLlXTPsO_K9WtHFGkHu1QlOcBYRTL9Iocy4z1qz12LihJrNr4YrEx-J5EE6RQXe0OHWspDURwvIVrnkRuve-xb7Y8qysZ1-KDQRxzqhf0BVlOmC9KZIFPnN6vBu4gJ7rN3P8naJFqBa2UdNzH0M69o7s01kZYSVZpgtnWwpR4JC0kDpgIygfQOcPWfNyA7XMIoETHb3IHDaHChTWcoAwm7_EsHcRkd6Whb3PQWfo="
 
-    TG_BOT_TOKEN = ""
+    TG_BOT_TOKEN = "1745605169:AAEsNvIS3zqP9sAlbnNmkq6wcUuy5YHbbr8"
 
-    PRIVATE_GROUP_BOT_API_ID = 
+    PRIVATE_GROUP_BOT_API_ID = -1001628008642
 
     COMMAND_HAND_LER = "."
 
