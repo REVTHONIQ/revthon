7a6767be01873ef0983d87f42aae2f59a802dca0
REVTHONIQ <123871565+REVTHONIQ@users.noreply.github.com>
2023-02-16 16:00:32 +0300

    Update ذاتية.py

--- revthon/plugins/ذاتية.py
+++ revthon/plugins/ذاتية.py
@@ -1,21 +1,34 @@
 from revthon import *
 from revthon import reviq
 from ..sql_helper.globals import gvarstatus
+from telethon import events
 
-@reviq.on(admin_cmd(pattern="(جلب الصورة|جلب الصوره|ذاتيه|ذاتية|حفظ)"))
-async def dato(event):
-    if not event.is_reply:
-        return await event.edit("..")
-    DEVREVV = await event.get_reply_message()
-    pic = await DEVREVV.download_media()
-    await bot.send_file(
-        "me",
-        pic,
-        caption=f"""
-- تـم حفظ الصـورة بنجـاح ✓ 
-- غير مبري الذمه اذا استخدمت الامر للابتزاز
-- CH: @Revthon
-- Dev: @DEVREVV
-  """,
-    )
-    await event.delete()
+smedia = False
+
+
+@reviq.admin_cmd(pattern="تفعيل الذاتية.")
+async def start_datea(event):
+    global smedia
+    smedia = True
+    await event.edit("- تم بنجاح تفعيل حفظ الميديا الذاتية من الان")
+
+@reviq.admin_cmd(pattern="تعطيل الذاتية.")
+async def stop_datea(event):
+    global smedia
+    smedia = False
+    await event.edit("- تم بنجاح تعطيل حفظ الميديا الذاتية من الان")
+
+@reviq.on(events.NewMessage(func=lambda e: e.is_private and (e.photo or e.video) and e.media_unread))
+async def handler(event):
+    global smedia
+    if smedia:
+        sender = await event.get_sender()
+        username = sender.username
+        user_id = sender.id
+
+        result = await event.download_media()
+        caption = (
+            f"ميديا ذاتية التدمير وصلت لك !\n: المرسل @{username}\nالايدي : {user_id}"
+        )
+        await event.send_file("me", result, caption=caption)
+    pass  
